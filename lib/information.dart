69e66d10a15d24e761307a8adc6f9b34c9c49b41
Talha Hussain <talhahussain056@gmail.com>
2023-12-10 13:50:16 +0500

    Flutter Grocery Shop

---
+++ lib/information.dart
@@ -0,0 +1,32 @@
+/*
+GestureDetector widget is used to wrap a Container. 
+The onTap property of GestureDetector listens for tap gestures on the wrapped Container. 
+When the container is tapped, it prints a message to the console.
+ 
+
+
+
+In Flutter, the elevation property is typically used with widgets like AppBar,
+Card, or Material to give them a shadow effect, 
+creating a sense of depth by making them appear raised from the background.
+
+
+Navigator.pushNamed is used to navigate to a new screen by specifying the route name
+previously defined in the app's route table.
+
+
+MainAxisAlignment.spaceBetween is a property in Flutter that 
+you can use within a Row or Column widget to define 
+how its children elements are spaced along the main axis.
+
+
+In Flutter, a text field is a widget used to enable users to input text
+
+
+
+const Expanded(
+            child: SizedBox(), // Adds empty space to push the TextField upwards
+          ),
+
+In menu.dart, Expanded widget with an empty SizedBox to push it up to the desired position within the Column
+*/
